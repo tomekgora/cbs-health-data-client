4bde04f2a898582b8b4edf8c968bc54e55f8af4d
tomekgora <tomekgora@gmail.com>
2020-01-14 10:29:54 +0100

    added user-logged in and group material icons

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -6,6 +6,8 @@ import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
+import PersonIcon from "@material-ui/icons/Person";
+import GroupIcon from "@material-ui/icons/Group";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -13,6 +15,10 @@ const useStyles = makeStyles(theme => ({
   },
   menuButton: {
     marginRight: theme.spacing(2)
+  },
+  userIcon: {
+    marginRight: theme.spacing(0),
+    marginLeft: theme.spacing(1)
   },
   title: {
     flexGrow: 1
@@ -28,16 +34,29 @@ export default function NavigationBar() {
         <Toolbar>
           <IconButton
             edge="start"
-            className={classes.menuButton}
+            className={classes.MenuIcon}
             color="inherit"
             aria-label="menu"
           >
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" className={classes.title}>
-            ...
+            Welcome to the page
           </Typography>
-          <Button color="inherit">Login</Button>
+          <IconButton
+            className={classes.userIcon}
+            color="inherit"
+            arial-label="logged-in"
+          >
+            <GroupIcon />
+          </IconButton>
+          <IconButton
+            className={classes.userIcon}
+            color="inherit"
+            arial-label="logged-in"
+          >
+            <PersonIcon />
+          </IconButton>
         </Toolbar>
       </AppBar>
     </div>
